3470ad982320b95e44d31a191e537c6749e42f17
abbaasalif <abbaasalif@yahoo.com>
2021-02-20 23:05:55 +0530

    brain DNN ready

--- brain.py
+++ brain.py
@@ -6,3 +6,18 @@
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras.optimizers import Adam
+
+
+# building the brain
+
+class Brain(object):
+
+    def __init__(self, learning_rate = 0.001, number_actions = 5):
+        self.learning_rate = learning_rate
+        states = Input(shape = (3,))
+        x = Dense(64, activation='sigmoid')(states)
+        y = Dense(32, activation='sigmoid')(x)
+        q_values = Dense(number_actions, activation="softmax")(y)
+        self.model = Model(inputs=states, outputs=q_values)
+        self.model.compile(loss = "mse", optimizer=Adam(lr = learning_rate))
+        
