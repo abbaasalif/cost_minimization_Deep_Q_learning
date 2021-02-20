a78453944c37844635933f6afab98f8bfe9fc863
abbaasalif <abbaasalif@yahoo.com>
2021-02-20 22:54:54 +0530

    added libraries for brain

--- brain.py
+++ brain.py
@@ -1,3 +1,8 @@
 #artificial intelligence for buisness case study-2
 #building the brain
 
+# importing the libraries
+
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras.optimizers import Adam
