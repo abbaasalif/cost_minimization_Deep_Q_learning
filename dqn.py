18932697cc52d452f02e21337d49cf26130c3fdc
abbaasalif <abbaasalif@yahoo.com>
2021-02-20 23:13:08 +0530

    completed intialization of DQN

--- dqn.py
+++ dqn.py
@@ -4,3 +4,14 @@
 # importing the libraries
 import numpy as np
 
+class DQN(object):
+
+    #Introduction and initialization of all parameters and variables of Deep Q Network
+    def __init__(self, max_memory = 100, discount = 0.9):
+        self.memory = list()
+        self.max_memory = max_memory
+        self.discount = discount
+
+    #Making a method that builds the memory in Experience Replay
+
+    # Making a method that builds two batches of 10 inputs and 10 targets by extracting 10 transitions
